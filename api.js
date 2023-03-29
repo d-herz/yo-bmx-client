fc45d07616124a1e3b101ddf92ebb6f550f09375
Daniel Hyres <danhyres@gmail.com>
2023-03-29 18:12:43 -0400

    added some placeholder data in api.js

---
+++ api.js
@@ -0,0 +1,34 @@
+export const forumPosts = {
+  1: {
+    id: 1,
+    title: "Forum post 1",
+    content: "Hey guys just wanted to say what's up!"
+  },
+  2: {
+    id: 2,
+    title: "Forum post 2",
+    content: "Hey guys just wanted to say what's up!"
+  },
+  3: {
+    id: 3,
+    title: "Forum post 3",
+    content: "Hey guys just wanted to say what's up!"
+  },
+  4: {
+    id: 4,
+    title: "Forum post 4",
+    content: "Hey guys just wanted to say what's up!"
+  },
+  5: {
+    id: 5,
+    title: "Forum post 5",
+    content: "Hey guys just wanted to say what's up!"
+  }, 
+}
+
+export const forumRooms = {
+  1: "General",
+  2: "Help and How to",
+  3: "Photos and Videos",
+  4: "For Sale",
+}
--- pages/forum/ForumRooms.jsx
+++ pages/forum/ForumRooms.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { forumRooms } from '../../api';
 
 function ForumRooms(props) {
   return (
